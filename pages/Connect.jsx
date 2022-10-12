01fc50dc1bc81ec9713107fcc653d8cef801bc8f
Sudip Bhusal <pidusbhusal@gmail.com>
2022-10-12 17:01:42 +0545

    connect section grid positioning

--- pages/Connect.jsx
+++ pages/Connect.jsx
@@ -1,18 +1,56 @@
 import React from "react";
 
-function Connect() {
-    const socials [{
-        "id":1 ,
-        "coonect_icon_address": "./mail.svg",
-        "arrow_adress" :"blackarrow.svg",
-    }
+function Connect_section() {
+  const socials = [
+    {
+      id: 1,
+      coonect_icon_address: "./mail.svg",
+      arrow_adress: "blackarrow.svg",
+      social_name: "hello@gmail.com",
+      class_name: "connect-email connect-social",
+    },
+    {
+      id: 2,
+      coonect_icon_address: "./mail.svg",
+      arrow_adress: "blackarrow.svg",
+      social_name: "Linkedin",
+      class_name: "connect-linkedin connect-social",
+    },
+    {
+      id: 3,
+      coonect_icon_address: "./mail.svg",
+      arrow_adress: "blackarrow.svg",
+      social_name: "Instagram",
+      class_name: "connect-instagram connect-social",
+    },
+    {
+      id: 4,
+      coonect_icon_address: "./mail.svg",
+      arrow_adress: "blackarrow.svg",
+      social_name: "Medium",
+      class_name: "connect-medium connect-social",
+    },
+    {
+      id: 5,
+      coonect_icon_address: "./mail.svg",
+      arrow_adress: "blackarrow.svg",
+      social_name: "Behance",
+      class_name: "connect-behance connect-social",
+    },
+  ];
 
-    ]
+  const bundle_social = socials.map((social) => (
+    <div className={social.class_name}>
+      <img src={social.coonect_icon_address} alt="" />
+      <div className="social-name">{social.social_name}</div>
+      <img src={social.arrow_adress} alt="" />
+    </div>
+  ));
   return (
     <div className="connect-section">
       <div className="connect-wrapper">
-        <h1>Say hi</h1>
-        <div className="connect_wrappper">
+        <h1 className="connect-title">Say hi</h1>
+        <div className="connect_button_wrappper">
           <div className="connect_button_wrapper">
             <input
               className="connect"
@@ -29,35 +67,14 @@ function Connect() {
           </p>
         </div>
         <div className="connect-link-wrapper">
-          <div className="connector-email">
-            <img src="" alt="" />
-            hello@pidus.com
-            <img src="" alt="" />
-          </div>
-          <div className="connector-linkedin">
-            <img src="" alt="" />
-            Linkedin
-            <img src="" alt="" />
-          </div>
-          <div className="connector-medium">
-            <img src="" alt="" />
-            Medium
-            <img src="" alt="" />
-          </div>
-          <div className="connector-instagram">
-            <img src="" alt="" />
-            Instagram
-            <img src="" alt="" />
-          </div>
-          <div className="connector-behance">
-            <img src="" alt="" />
-            Behance
-            <img src="" alt="" />
-          </div>
+          {bundle_social}
+          <div className="divider-line email-divider"></div>
+          <div className="divider-line linkedin-divider"></div>
+          <div className="divider-line instagram-divider"></div>
         </div>
       </div>
     </div>
   );
 }
 
-export default Connect;
+export default Connect_section;
--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,17 @@ import { Navbar } from "./Navbar";
 import { Work } from "./Work";
 import "../styles/main.css";
 import Aboutme from "./Aboutme";
+import Connect_section from "./Connect";
 
 function App() {
   return (
     <div className="App">
-      <div className="animating_background">
-        
-      </div>
+      <div className="animating_background"></div>
       <Navbar />
       <Herosection className="Herosection" />
       <Work />
-      <Aboutme/>
+      <Aboutme />
+      <Connect_section />
     </div>
   );
 }
